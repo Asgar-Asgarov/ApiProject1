45d87e14bc9d1d8de4f06e0ff181473df5f49542
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-04-06 14:39:09 +0400

    feature:Services added

--- ApiCrud/Data/DAL/AppDbContext.cs
+++ ApiCrud/Data/DAL/AppDbContext.cs
@@ -1,1 +1,13 @@
-namespace ApiCrud.DAL;
+using Microsoft.EntityFrameworkCore;
+using ApiCrud.Models;
+
+namespace ApiCrud.Data.DAL;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Product> Products { get; set; }
+}
