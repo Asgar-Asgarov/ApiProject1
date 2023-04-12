31c5d6771c7ef985af4e52d08be97129fbc32765
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2023-04-12 08:05:49 +0400

    feature:CategoryId added for ProductCreateDto

--- ApiCrud/Dtos/ProductDtos/ProductCreateDto.cs
+++ ApiCrud/Dtos/ProductDtos/ProductCreateDto.cs
@@ -2,7 +2,8 @@ using FluentValidation;
 namespace ApiCrud.Dtos;
 
 public class ProductCreateDto
-{
+{  
+    public int? CategoryId { get; set; }
     public string? Name { get; set; }
     public IFormFile? Photo { get; set; }
     public double Price { get; set; }
@@ -13,6 +14,9 @@ public class ProductCreateDtoValidator : AbstractValidator<ProductCreateDto>
 {
     public ProductCreateDtoValidator()
     {
+        RuleFor(p=>p.CategoryId)
+        .GreaterThanOrEqualTo(1)
+        .NotNull().WithMessage("Bosh ola bilmez");
         RuleFor(p => p.Name)
         .MaximumLength(50).WithMessage("50 den boyuk olmaz")
         .NotNull().WithMessage("Bosh qoymaq olmaz");
